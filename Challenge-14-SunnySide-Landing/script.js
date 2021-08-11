64c66cc7c3a3c837c9ba6f8d14a17df772523d7b
revanthreddy673 <revanthreddy673@gmail.com>
2021-08-11 13:24:51 +0530

    Smooth transition

--- Challenge-14-SunnySide-Landing/script.js
+++ Challenge-14-SunnySide-Landing/script.js
@@ -3,6 +3,13 @@
 const navBar = document.querySelector(".nav-bar");
 const navIcon = document.querySelector(".nav-icon");
 const mobileNav = document.querySelector(".mobile-nav");
+const navBarList = document.querySelector(".nav-bar-list");
+const footerList = document.querySelector(".nav-list");
+const mobileNavBar = document.querySelector(".mobile-nav-list");
+const servicesSection = document.querySelector("#services-section");
+const projectSection = document.querySelector("#projectSC");
+const contactSection = document.querySelector("#contactFr");
+const headerSection = document.querySelector("#headerFc");
 
 class App {
   constructor() {
@@ -16,6 +23,52 @@ class App {
     navIcon.addEventListener("click", function () {
       mobileNav.classList.toggle("nav-out-of-view");
     });
+
+    navBarList.addEventListener("click", this._scrollListener.bind(this));
+
+    footerList.addEventListener("click", this._scrollListener.bind(this));
+
+    mobileNavBar.addEventListener("click", this._scrollListener.bind(this));
+  }
+
+  _scrollListener(event) {
+    console.log(event);
+    event.preventDefault();
+    if (event.target.textContent.toLowerCase() == "Services".toLowerCase()) {
+      const positions = servicesSection.getBoundingClientRect();
+      window.scrollTo({
+        left: positions.left + window.pageXOffset,
+        top: positions.top + window.pageYOffset,
+        behavior: "smooth",
+      });
+    } else if (
+      event.target.textContent.toLowerCase() == "Projects".toLowerCase()
+    ) {
+      const positions = projectSection.getBoundingClientRect();
+      window.scrollTo({
+        left: positions.left + window.pageXOffset,
+        top: positions.top + window.pageYOffset,
+        behavior: "smooth",
+      });
+    } else if (
+      event.target.textContent.toLowerCase() == "Contact".toLowerCase()
+    ) {
+      const positions = contactSection.getBoundingClientRect();
+      window.scrollTo({
+        left: positions.left + window.pageXOffset,
+        top: positions.top + window.pageYOffset,
+        behavior: "smooth",
+      });
+    } else if (
+      event.target.textContent.toLowerCase() == "About".toLowerCase()
+    ) {
+      const positions = headerSection.getBoundingClientRect();
+      window.scrollTo({
+        left: positions.left + window.pageXOffset,
+        top: positions.top + window.pageYOffset,
+        behavior: "smooth",
+      });
+    }
   }
 
   _desktopinit() {
